7ee108df1456bfdf9e327754991d372850d05e1d
Lucas Cavalheiro <lucasribeiro120@hotmail.com>
2022-08-08 14:31:06 -0300

    Resolve error 'Content' not finded

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { ArtistProvider, ResultProvider } from 'contexts';
 import Router from 'routes/router';
 import { GlobalStyle } from "./assets/style/global";
 import { ContainerCatch } from "./routes";
-import Content from './routes/SearchScreen';
 
 function App() {
   return (
